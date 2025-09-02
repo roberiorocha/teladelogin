ec1945a8d78472f50906d70bf8694390f802a9c2
roberiorocha <roberiorocha@gmail.com>
2025-09-02 17:53:27 -0300

    Ajustes na pagina princial

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,15 +5,17 @@ form.addEventListener('submit', (e) => {
   const username = document.getElementById('username').value;
   const password = document.getElementById('password').value;
 
+  const usuario = { nome: "Roberio", foto: "" }
+  //simulação de dados do usuário logado - atualiza os dados do usuário na tela
+
   // Simulação de autenticação
-   if (username === 'admin' && password === '123') {
+  if (username === 'admin' && password === '123') {
     alert('Login efetuado com sucesso!');
-    // Redirecionar para a página principal
+    // Redirecionar para a página principal do usuário logado
     window.location.href = 'assets/html/pagina-principal.html';
   } else {
     alert('Usuário ou senha inválidos!');
   }
-
 });
 
 
--- assets/js/pagina-principal.js
+++ assets/js/pagina-principal.js
@@ -1,10 +1,21 @@
-const form = document.getElementById('register-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();  
-    //Redirecionar para a página inicial
-    window.location.href = '/index.html';
-  });
+// Simulação de dados do usuário
+const usuario = {
+      nome: "João Pedro",
+      foto: ""
+    };
 
+    // Atualiza o nome do usuário na tela
+    document.getElementById("nome-usuario").innerText = usuario.nome;
+    document.getElementById("nome-usuario-direita").innerText = usuario.nome;
 
+    // Função para carregar a foto do usuário
+    document.getElementById("foto-usuario").addEventListener("change", (e) => {
+      const file = e.target.files[0];
+      const reader = new FileReader();
+      reader.onload = () => {
+        document.getElementById("foto-usuario-preview").src = reader.result;
+      };
+      reader.readAsDataURL(file);
+    });
 
